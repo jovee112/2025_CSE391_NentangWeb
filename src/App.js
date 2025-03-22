8c492c1e5580780ab47480b7ff2f6ce7964f1309
lwlwlwlwsss <luwuluyeens@gmail.com>
2025-03-22 11:39:55 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [color, setColor] = useState("");
   const [counter, setCounter] = useState(0);
 
   const incrementCounter = () => {
@@ -17,12 +18,21 @@ function App() {
   };
 
   return (
-    <div className="state">
-      <h1>Counter: {counter}</h1>
-      <div classname="item">
-        <button className="add-btn" onClick={incrementCounter}>Increment</button>
-        <button className="delete-btn" onClick={decrementCounter}>Decrement</button>
-        <button className="reset-btn" onClick={resetCounter}>Reset</button>
+    <div className="App" style={{ backgroundColor: color }}>
+      <select onChange={(e) => setColor(e.target.value)}>
+        <option value="Red">Red</option>
+        <option value="Green">Green</option>
+        <option value="Blue">Blue</option>
+        <option value="Yellow">Yellow</option>
+        <option value="Orange">Orange</option>
+      </select>
+      <div className="state">
+        <h1>Counter: {counter}</h1>
+        <div className="item">
+          <button className="add-btn" onClick={incrementCounter}>Increment</button>
+          <button className="delete-btn" onClick={decrementCounter}>Decrement</button>
+          <button className="reset-btn" onClick={resetCounter}>Reset</button>
+        </div>
       </div>
     </div>
   );
